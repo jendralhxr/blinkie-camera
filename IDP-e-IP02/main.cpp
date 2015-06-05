f260530aef114110d5378be5ff0c8bb40f058f4a
jendralhxr <jendral.hxr@gmail.com>
2015-06-05 13:48:31 +0900

    parity check and frame waiting using goto

--- IDP-e-IP02/main.cpp
+++ IDP-e-IP02/main.cpp
@@ -23,9 +23,9 @@
 #define SHUTTER		56000
 #define IMG_WIDTH	512
 #define IMG_HEIGHT	48
-#define FRAMENUM_MAX 200
+#define FRAMENUM_MAX 400
 #define LUMINANCE_THRESHOLD	60
-#define DELAY_UNIT 10
+#define DELAY_UNIT 100
 
 using namespace IDPExpress;
 using namespace mytimer;
@@ -55,7 +55,9 @@ Mat *img;
 
 char value_current, value_temp, value_prev;
 char buffer[256];
-char state; // 0 waiting; 1 read
+int nframenumber[FRAMENUM_MAX];
+
+// char state; // 0 waiting; 1 read
 int index_char= 0;
 std::string str;
 
@@ -64,8 +66,9 @@ ofstream logfile;
 char filename[20];
 
 // self tuning
-int axisY=8;
-int axisX[8]={329, 343, 359, 374, 389, 404, 419, 433};
+int axisY=7;
+int axisX[8]={103, 117, 131, 144, 157, 171, 185, 198};
+bool state[8];
 
 unsigned char max_global, min_global, max_current, min_current, clk_intensity;
 unsigned int delay;
@@ -77,41 +80,39 @@ int main(){
 	if (idpConf.setRecordRate(FPS)						== PDC_FAILED) return 1;
 	if (idpConf.setShutterSpeed(SHUTTER)				== PDC_FAILED) return 1;
 	if (idpConf.setResolution(IMG_WIDTH, IMG_HEIGHT)	== PDC_FAILED) return 1;
-
 	if (idpConf.setPixelGainMode()						== PDC_FAILED) return 1;
 	if (idpConf.setLiveStatus()							== PDC_FAILED) return 1;
 	if (idpConf.getRemainBlocks()						== PDC_FAILED) return 1;	
-
-	if(idpConf.setRecordingBlocks(1)					== PDC_FAILED) return 1;
-	if (idpConf.getMaxFrames()							== PDC_FAILED) return 1;	
-	if(idpConf.getMaxFrames()							== PDC_FAILED) return 1;
-	if(idpConf.setRecReady()							== PDC_FAILED) return 1;
-	if(idpConf.setTriggerMode(  0, PDC_TRIGGER_MANUAL,
+	if (idpConf.setRecordingBlocks(1)					== PDC_FAILED) return 1;
+	if (idpConf.getMaxFrames()							== PDC_FAILED) return 1;
+	if (idpConf.setRecReady()							== PDC_FAILED) return 1;
+	if (idpConf.setTriggerMode(  0, PDC_TRIGGER_MANUAL,
 		idpConf.getNumFrames(),
 		idpConf.getNumFrames(),
 		1)						== PDC_FAILED) return 1;
-	if(idpConf.setTriggerMode(  1, PDC_TRIGGER_MANUAL,
-		idpConf.getNumFrames(),
-		idpConf.getNumFrames(),
-		1)						== PDC_FAILED) return 1;
-	if(idpConf.startEndlessRecording()					== PDC_FAILED) return 1;
+	//if (idpConf.setTriggerMode(  1, PDC_TRIGGER_MANUAL,
+	//	idpConf.getNumFrames(),
+	//	idpConf.getNumFrames(),
+	//	1)						== PDC_FAILED) return 1;
+	if (idpConf.startEndlessRecording()					== PDC_FAILED) return 1;
 
 	IDPExpressUtil idpUtil(idpConf.getHeadNumber(0), IMG_WIDTH, IMG_HEIGHT);
 
 	int colortype_cam1 = idpConf.getCameraType(0,0);
-	colortype_cam1=1;
+	int colortype_cam2 = idpConf.getCameraType(0,1);
+
 	// color mode
 	// cam1_height | cam1_color_mode | cam1_output_mode
 	// 16 bit | 8 bit | 8 bit
 	idpConf.writeRegister(0, ADDR_MOD_CAM1, ((IMG_HEIGHT-1)<<16) | (colortype_cam1<<8) | 0x00);
+	idpConf.writeRegister(0, ADDR_MOD_CAM2, ((IMG_HEIGHT-1)<<16) | (colortype_cam1<<8) | 0x00);
 
-	if(colortype_cam1 == 1){
-		idpConf.writeRegister(0, ADDR_THRE_HSV_CAM1, 0x00ff0000);
-	} else {
-		// neglect 'slightly darker'
-		//idpConf.writeRegister(0, ADDR_THRE_HSV_CAM1, 0x00ff)
-		idpConf.writeRegister(0, ADDR_THRE_HSV_CAM1, 0x10ff);
-	}
+	if(colortype_cam1 == 1) idpConf.writeRegister(0, ADDR_THRE_HSV_CAM1, 0x00ff0000);
+	else idpConf.writeRegister(0, ADDR_THRE_HSV_CAM1, 0x00ff);
+	if(colortype_cam2 == 1) idpConf.writeRegister(0, ADDR_THRE_HSV_CAM2, 0x00ff0000);
+	else idpConf.writeRegister(0, ADDR_THRE_HSV_CAM2, 0x00ff);
+
+	// mess start here
 
 	int framenum;
 	imgHead			= Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
@@ -120,9 +121,6 @@ int main(){
 		img[framenum] = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
 	}
 	
-	Timer tm;
-	tm.init();	
-
 	unsigned long	nFrameNo, oldFrameNo = 0;
 	void			*pBaseAddress;
 
@@ -130,19 +128,24 @@ int main(){
 	logfile << "start " << GetTickCount() << endl;;
 	//logfile.close();
 
-	state=0;
-	while(1){
-		if( idpConf.getLiveFrameAddress(0, &nFrameNo, &pBaseAddress) == PDC_FAILED ) break;
-		if(oldFrameNo != 0 && nFrameNo == oldFrameNo) continue;
-		oldFrameNo = nFrameNo;
+	delay= 600;
+	idpConf.writeRegister(0, 0xb4, delay, 0);
 
+	// state=0;
+	for (framenum=0; framenum<FRAMENUM_MAX; framenum++){
+	//while(1){
+	waitframe:
+		if (idpConf.getLiveFrameAddress(0, &nFrameNo, &pBaseAddress) == PDC_FAILED ) break;
+		if (nFrameNo == oldFrameNo) goto waitframe;
+
+		oldFrameNo = nFrameNo;
+		//logfile << framenum << "--" << nFrameNo << endl;
+	
 		idpUtil.setBase((UINT8 *)pBaseAddress + 8);
 
-		idpUtil.getHeadData(imgHead.data, 1);
-		//idpUtil.getHeadData(img[framenum].data, 1);
-		//imshow("Result", img[framenum]);
-		//imshow("Result", imgHead);
-
+		//idpUtil.getHeadData(imgHead.data, 1);
+		idpUtil.getHeadData(img[framenum].data, 1);
+		
 		// read current frame's character value (iterative)
 		/*value_temp= 0;
 		for (int n=0; n<8; n++){
@@ -152,22 +155,29 @@ int main(){
 		}*/
 		
 		value_temp= 0;
-		//if (imgHead.data[(axisY*IMG_WIDTH + axisX[7])] > LUMINANCE_THRESHOLD) {
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[0])] > LUMINANCE_THRESHOLD) value_temp |= 1;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[1])] > LUMINANCE_THRESHOLD) value_temp |= 2;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[2])] > LUMINANCE_THRESHOLD) value_temp |= 4;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[3])] > LUMINANCE_THRESHOLD) value_temp |= 8;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[4])] > LUMINANCE_THRESHOLD) value_temp |= 16;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[5])] > LUMINANCE_THRESHOLD) value_temp |= 32;
-			if (imgHead.data[(axisY*IMG_WIDTH + axisX[6])] > LUMINANCE_THRESHOLD) value_temp |= 64;
-		//}
+		for (int i=0; i<8; i++){
+			state[i] = FALSE;
+			if (img[framenum].data[(axisY*IMG_WIDTH + axisX[i])] > LUMINANCE_THRESHOLD) state[i] = TRUE;
+		}
+				
+		if (state[0]) value_temp |= 1;
+		if (state[1]) value_temp |= 2;
+		if (state[2]) value_temp |= 4;
+		if (state[3]) value_temp |= 8;
+		if (state[4]) value_temp |= 16;
+		if (state[5]) value_temp |= 32;
+		if (state[6]) value_temp |= 64;
+
 		//temporal character being read
+		// add (even) parity check
 		// logfile << framenum << " reads " << char(value_temp) << '(' << int(value_temp) << ')' <<  endl;
-		if ((value_temp!=value_prev) && value_temp){
+		if ((value_temp!=value_prev) && value_temp && \
+			(state[0]^state[1]^state[2]^state[3]^state[4]^state[5]^state[6]^state[7]^TRUE)){
 			logfile << char(value_temp);
 			value_prev= value_temp;
 		}
 		
+		/*
 		// dummy pll function here
 		clk_intensity= imgHead.data[(axisY*IMG_WIDTH + axisX[7])];
 		if (clk_intensity > max_current){
@@ -179,7 +189,7 @@ int main(){
 			state_fall= TRUE;
 		}
 		if (state_rise && state_fall){
-			if ((max_current-min_current) > (max_global-min_global)){
+			if ((max_current-min_current) >= (max_global-min_global)){
 				max_global= max_current;
 				min_global= min_current;
 				min_current= 255;
@@ -196,7 +206,7 @@ int main(){
 			}
 		}
 		// shutter speed delay
-		idpConf.writeRegister(0, 0xb4, delay, 0);
+		*/
 		
 
 		/*
@@ -233,11 +243,15 @@ int main(){
 			//cout << framenum << " reads zero" << endl;
 		}
 		*/
-		framenum++;
+		
+	
 	}
 
 	//logfile.open("logtime-fixedpoints.txt", ios::app);
+	
+
 	logfile << endl << "stop  " << GetTickCount() << endl;;
+	
 	logfile << "------------" << endl;;
 	logfile.close();
 	
@@ -245,21 +259,19 @@ int main(){
 	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 	compression_params.push_back(9);
 
-	
-	/*
 	for(framenum=0; framenum<FRAMENUM_MAX; framenum++){
 		try {
 			sprintf(filename,"%6.6d.jpg",framenum);
 			// for (int i=0; i<IMG_WIDTH; i++) img[framenum].data[axisY*IMG_WIDTH + i] = 200;
-			// imwrite(filename, img[framenum], compression_params);
-			imwrite(filename, img[framenum]);
+			// imwrite(filename, img[framenum], compression_params); 
+			// imwrite(filename, img[framenum]);
 
 		}
 		catch (runtime_error& ex) {
 			fprintf(stderr, "Exception converting image to JPG format: %s\n", ex.what());
 			return 1;
 		}
-		
+		/*
 		logfile.open("logdata-fixedpoints.txt", ios::app);
 		logfile << int(img[framenum].data[(axisY*IMG_WIDTH + axisX[0])]) << ';' << \
 			int(img[framenum].data[axisY*IMG_WIDTH + axisX[1]]) << ';' << \
@@ -271,11 +283,11 @@ int main(){
 			int(img[framenum].data[axisY*IMG_WIDTH + axisX[7]]) << ';';
 		logfile << endl;
 		logfile.close();
+		*/
+		
 		
 	}
 	
-	*/
-
 	if (idpConf.closeDevice() == PDC_FAILED) return 1;
 	destroyAllWindows();
 	return 0;
