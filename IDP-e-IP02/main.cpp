d57acc254ef7c71b938be64802f596ce79b2cc9a
jendralhxr <jendral.hxr@gmail.com>
2015-12-22 14:03:50 +0900

    external_in and longer detection sequence (which shorter is desired)

--- IDP-e-IP02/main.cpp
+++ IDP-e-IP02/main.cpp
@@ -20,7 +20,7 @@
 #pragma comment(lib, "PDCLIB.lib")
 
 #define FPS			2000
-#define SHUTTER		FPS
+#define SHUTTER		2000
 #define IMG_WIDTH	512
 #define IMG_HEIGHT	512
 
@@ -86,23 +86,36 @@ unsigned int delay, delay_freq, delay_phase;
 char value_temp[2];
 unsigned char lumi_temp[2], lumi_max[2], lumi_min[2];
 // self-tuning position
-int axisY=246;
-int axisX[8]={466, 435, 403, 372, 342, 310, 279, 248};
+int axisY=220;
+int axisX[8]={316, 284, 252, 221, 191, 159, 129, 98};
 bool state[8];
-int axisY2=246;
-int axisX2[8]={466, 435, 403, 372, 342, 310, 279, 248};
+int axisY2=221;
+int axisX2[8]={316, 284, 252, 221, 191, 159, 129, 98};
 bool state2[8];
 
 // for PLL
 unsigned int segment_start, segment_end, segment_iter, segment_period, segment_period_temp;
 char segment_sequence; // consecutive 8 frames marker
-bool sync_state_cur, sync_state_prev[7];
+bool sync_current, sync_prev[24];
+
+void check_lumi0(){
+	if (lumi_temp[0] > lumi_max[0]) lumi_max[0]= lumi_temp[0];
+	else if (lumi_temp[0] < lumi_min[0]) lumi_min[0]= lumi_temp[0];
+}
 
 int main(){
 	if (idpConf.init()									== PDC_FAILED) return 1;
 	if (idpConf.setRecordRate(FPS)						== PDC_FAILED) return 1;
 	if (idpConf.setShutterSpeed(SHUTTER)				== PDC_FAILED) return 1;
 	if (idpConf.setResolution(IMG_WIDTH, IMG_HEIGHT)	== PDC_FAILED) return 1;
+	
+	unsigned long errorcode=0;
+	unsigned long modenum, modelist;
+
+	//PDC_GetTriggerModeList(1, &modenum, &modelist, &errorcode);
+	//PDC_SetExternalInMode(0, 0, PDC_EXT_IN_TRIGGER_POSI, &errorcode); 
+ 	//if(idpConf.setExternalInMode(0)						== PDC_FAILED) return 14; // external sync
+	
 	if (idpConf.setPixelGainMode()						== PDC_FAILED) return 1;
 	if (idpConf.setLiveStatus()							== PDC_FAILED) return 1;
 	if (idpConf.getRemainBlocks()						== PDC_FAILED) return 1;	
@@ -118,9 +131,8 @@ int main(){
 	//	idpConf.getNumFrames(),
 	//	1)						== PDC_FAILED) return 1;
 	if (idpConf.startEndlessRecording()					== PDC_FAILED) return 1;
-
+	
 	IDPExpressUtil idpUtil(idpConf.getHeadNumber(0), IMG_WIDTH, IMG_HEIGHT);
-
 	int colortype_cam1 = idpConf.getCameraType(0,0);
 	int colortype_cam2 = idpConf.getCameraType(0,1);
 
@@ -163,8 +175,8 @@ int main(){
 		if (idpConf.getLiveFrameAddress(0, &nFrameNo, &pBaseAddress) == PDC_FAILED ) break;
 		if (nFrameNo == oldFrameNo) goto waitframe;
 		oldFrameNo = nFrameNo;
-		//logfile << framenum << "--" << nFrameNo << endl;
-		idpUtil.setBase((UINT8 *)pBaseAddress -8);
+		//cout << framenum << "--" << nFrameNo << endl;
+		idpUtil.setBase((UINT8 *)pBaseAddress +8);
 		//idpUtil.getHeadData(imgHead.data, 1);
 		idpUtil.getHeadData(imgHead.data, 0);
 		
@@ -172,7 +184,6 @@ int main(){
 		//for later saving
 		imgHead.copyTo(img[framenum]);
 #endif
-
 		// for source1
 		// current character being read
 		value_temp[0]= 0;
@@ -181,24 +192,33 @@ int main(){
 		
 		for (int i=0; i<8; i++){
 			state[i] = FALSE;
+			// selection isn't else'd so execution is rather constant
 			lumi_temp[0]= imgHead.data[(axisY*IMG_WIDTH + axisX[i])];
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
+			check_lumi0();
 			lumi_temp[0]= imgHead.data[(axisY*IMG_WIDTH + axisX[i]+1)];
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
+			check_lumi0();
 			lumi_temp[0]= imgHead.data[(axisY*IMG_WIDTH + axisX[i]-1)];
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
+			check_lumi0();
 			lumi_temp[0]= imgHead.data[((axisY+1)*IMG_WIDTH + axisX[i])];
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
 			lumi_temp[0]= imgHead.data[((axisY+1)*IMG_WIDTH + axisX[i]+1)];
+			check_lumi0();
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
 			lumi_temp[0]= imgHead.data[((axisY+1)*IMG_WIDTH + axisX[i]-1)];
+			check_lumi0();
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
 			lumi_temp[0]= imgHead.data[((axisY-1)*IMG_WIDTH + axisX[i])];
+			check_lumi0();
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
 			lumi_temp[0]= imgHead.data[((axisY-1)*IMG_WIDTH + axisX[i]+1)];
+			check_lumi0();
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
 			lumi_temp[0]= imgHead.data[((axisY-1)*IMG_WIDTH + axisX[i]-1)];
 			if (lumi_temp[0] > THRESHOLD_LOGIC) state[i] = TRUE;
+			check_lumi0();
 			//if (lumi_temp[0] > lumi_max[0]) lumi_max[0]= lumi_temp[0];
 			//else if (lumi_temp[0] < lumi_min[0]) lumi_min[0]= lumi_temp[0];
  		}
@@ -220,6 +240,7 @@ int main(){
 
 		for (int i=0; i<8; i++){
 			state2[i] = FALSE;
+			// selection isn't else'd so execution is rather constant
 			lumi_temp[1]= imgHead.data[(axisY2*IMG_WIDTH + axisX2[i])];
 			if (lumi_temp[1] > THRESHOLD_LOGIC) state2[i] = TRUE;
 			lumi_temp[1]= imgHead.data[(axisY2*IMG_WIDTH + axisX2[i]+1)];
@@ -254,23 +275,30 @@ int main(){
 		// yet another silly PLL, based on readable segment, source1
 		// detecting readable segment
 		segment_sequence = segment_sequence <<1;
-		sync_state_cur= FALSE;
+		sync_current= FALSE;
 		if (state[0]^state[1]^state[2]^state[3]^state[4]^state[5]^state[6]^state[7]^TRUE){
 			logfile <<value_temp[0];
 			segment_sequence |= 1;
 			}
-		if ((segment_sequence&0xFF)==0xFF){ // just what??
-		sync_state_cur=TRUE;
-		}
-		// deciding readable segment period, start and end frames 	
-		if ((sync_state_cur==TRUE) && (sync_state_prev[0]==TRUE) && (sync_state_prev[1]==TRUE) && \
-			(sync_state_prev[2]==TRUE) && (sync_state_prev[3]==TRUE) && (sync_state_prev[4]==TRUE) &&
-			(sync_state_prev[5]=TRUE) && (sync_state_prev[6]==FALSE)){
+		if ((segment_sequence&0xFF)==0xFF) sync_current=TRUE; // still no parity error for 8 frames
+		// deciding readable segment period, start and end frames
+		
+		// 15 frames cleared
+		if ((sync_current==TRUE) && (sync_prev[0]==TRUE) && (sync_prev[1]==TRUE) && \
+			(sync_prev[2]==TRUE) && (sync_prev[3]==TRUE) && (sync_prev[4]==TRUE) && \
+			(sync_prev[5]==TRUE) && (sync_prev[6]==TRUE) && (sync_prev[7]==TRUE) && \
+			(sync_prev[8]==TRUE) && (sync_prev[9]==TRUE) && (sync_prev[10]==TRUE) && \
+			(sync_prev[11]==TRUE) && (sync_prev[12]==TRUE) && (sync_prev[13]==TRUE) && \
+			(sync_prev[13]==TRUE) && (sync_prev[15]==TRUE) && (sync_prev[16]==TRUE) && \
+			(sync_prev[16]==TRUE) && (sync_prev[18]==TRUE) && (sync_prev[19]==TRUE) && \
+			(sync_prev[19]==TRUE) && (sync_prev[21]==TRUE) && (sync_prev[22]==TRUE) && \
+			(sync_prev[22]=TRUE) && (sync_prev[23]==FALSE)){
 			if ((segment_start==0) && (segment_end==0)) segment_start= framenum;
 			else {
 				segment_end= framenum;
 				segment_period_temp= segment_end - segment_start;
-				if (segment_period_temp>segment_period) segment_period=segment_period_temp;
+				//if (segment_period_temp>segment_period) segment_period=segment_period_temp; // get the biggest
+				segment_period=segment_period_temp; // get the biggest
 				segment_start= framenum;
 				segment_end= framenum + segment_period;
 			}
@@ -283,38 +311,35 @@ int main(){
 		else if (segment_period) delay_phase= int((framenum-segment_start)*SEGMENT_COUNT/segment_period);
 		switch(delay_phase){
 		case 1:
-			idpConf.writeRegister(0, 0xb4, 10100, 0);
+	//		idpConf.writeRegister(0, 0xb4, 10100, 0);
 			break;
 		case 2:
-			idpConf.writeRegister(0, 0xb4, 20200, 0);
+//			idpConf.writeRegister(0, 0xb4, 20200, 0);
 			break;
 		case 3:
-			idpConf.writeRegister(0, 0xb4, 30300, 0);
+	//		idpConf.writeRegister(0, 0xb4, 30300, 0);
 			break;
 		case 4:
-			idpConf.writeRegister(0, 0xb4, 40400, 0);
+	//		idpConf.writeRegister(0, 0xb4, 40400, 0);
 			break;
 		default: // including 0
-			idpConf.writeRegister(0, 0xb4, 0, 0);
+	//		idpC  onf.writeRegister(0, 0xb4, 0, 0);
 			break;
 		}
 
-		// intensity logging
-		//logintensity << int(lumi_min[0]) << ';' << int(lumi_max[0]) << ';' << int(lumi_min[1]) << ';' << int(lumi_max[1]) << endl;
+		// more logging
+		//logintensity << sync_current<<int(framenum-segment_start) << '/' << segment_period << endl;
 		//logintensity << int(lumi_min[0]) << ';' << int(lumi_max[0]) << ';' << int(lumi_temp[0]) <<';' << delay_phase << endl;
-		logintensity << int(lumi_temp[0]) <<';'<< std::hex << short int(segment_sequence&0xFF) << std::dec << ';' << sync_state_cur << ';' << delay_phase << ';' << int(framenum-segment_start) << '/' << segment_period << endl;
+		
+		logintensity << sync_current << ';' << short int(lumi_min[0]) << ';' << short int(lumi_max[0]) << ';' << int(framenum-segment_start) << ';' << segment_period << endl;
+		//logintensity << sync_current << ';' << lumi_min[0] << ';' << lumi_max[0] << ';' << int(framenum-segment_start) << ';' << segment_period << endl;
+		
 		// result logging
-		//logfile << value_temp[0];
-		logfile2 << value_temp[1];
+		logfile2 << value_temp[1]; // without parity check
 		if (!(framenum%500)) logfile << endl << 'q' <<framenum << endl;
 		
-		sync_state_prev[6]= sync_state_prev[5];
-		sync_state_prev[5]= sync_state_prev[4];
-		sync_state_prev[4]= sync_state_prev[3];
-		sync_state_prev[3]= sync_state_prev[2];
-		sync_state_prev[2]= sync_state_prev[1];
-		sync_state_prev[1]= sync_state_prev[0];
-		sync_state_prev[0]= sync_state_cur;
+		for (int i=23; i>0; i--) sync_prev[i]= sync_prev[i-1];
+		sync_prev[0]= sync_current;
 		
 	} // framenum
 
