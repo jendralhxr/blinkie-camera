866977b98957d39d0af5cc52dfc2094d714d2235
jendralhxr <jendral.hxr@gmail.com>
2016-05-18 17:54:08 +0900

    DLP bit plane reconstruction

--- IDP-e-IP02/main.cpp
+++ IDP-e-IP02/main.cpp
@@ -1,12 +1,14 @@
 #include "IDPExpressUtil.h"
 #include "IDPExpressConfig.h"
 #include "iocsv.hpp"
-#include "timer.h"
+//#include "timer.h"
+#include "windows.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <ctime>
 
 #ifdef _DEBUG
 #pragma comment(lib, "opencv_core247d.lib")
@@ -24,15 +26,15 @@
 #define IMG_WIDTH	512
 #define IMG_HEIGHT	512
 
-//#define OPT_SAVE 
+#define OPT_SAVE 
 #ifdef OPT_SAVE
-#define FRAMENUM_MAX 40
+#define FRAMENUM_MAX 800
 #else
 #define FRAMENUM_MAX 4000
 #endif
 
 using namespace IDPExpress;
-using namespace mytimer;
+//using namespace mytimer;
 using namespace std;
 using namespace cv;
 
@@ -54,12 +56,15 @@ IDPExpressConfig idpConf(1);
 #define ADDR_ROI1_CAM2		0x30
 #define ADDR_ROI2_CAM2		0x34
 
-Mat	imgHead;
+#define THRESHOLD_BACKGROUND 30
+#define THRESHOLD_LOGIC 90
+bool background, background_prev;
+
+Mat	imgHead, imgResult;
 Mat *img;
 
 char buffer[256];
 int nframenumber[FRAMENUM_MAX];
-
 int index_char= 0;
 std::string str;
 
@@ -67,39 +72,35 @@ std::string str;
 ofstream logintensity;
 char filename[20];
 
-#define SECTIONNUM 13
-#define LUMI_INIT 230
-unsigned char value_temp[2], value_prev[2], value_prev2[2], value_current[2];
-unsigned char lumi_temp[2], lumi_max[2], lumi_min[2], lumi_led[9], lumi_threshold=LUMI_INIT;
-// self-tuning position
-int axisX[SECTIONNUM]={191,219,288,341,364,346,290,223,156,103, 81, 89,148};
-int axisY[SECTIONNUM]={217, 85, 96,142,215,283,330,349,329,290,215,139, 89};
-bool state[8];
+//timer
+LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
+LARGE_INTEGER Frequency;
 
-bool sync_current;
+//DLP stuff
+#define SEQUENCE_MAX 33
+unsigned short int sequence;
+unsigned short int \
+ bitplane[SEQUENCE_MAX]={7, 6, 4, 4, 3, 2, 1, 0,\
+						 0, 0, 0, 0, 0, 0, 0, 0,\
+						 0, 0, 0, 0, 0, 0, 0, 0,\
+						 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-// for PLL
-#define MARKER_BLOCK 100
-#define DELAY_STEP 8000 // from 0 to 50000-ish
-float marker_accu, marker_prev=255;
-unsigned int marker_iter;
-unsigned int delay_phase;
-bool marker_stop;
 
-void check_lumi0(){
-	if (lumi_temp[0] > lumi_max[0]) lumi_max[0]= lumi_temp[0];
-	else if (lumi_temp[0] < lumi_min[0]) lumi_min[0]= lumi_temp[0];
-}
 
 int main(){
+	QueryPerformanceFrequency(&Frequency); 
+	//saving as PNG
+	vector<int> compression_params;
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+
 	if (idpConf.init()									== PDC_FAILED) return 1;
 	if (idpConf.setRecordRate(FPS)						== PDC_FAILED) return 1;
 	if (idpConf.setShutterSpeed(SHUTTER)				== PDC_FAILED) return 1;
 	if (idpConf.setResolution(IMG_WIDTH, IMG_HEIGHT)	== PDC_FAILED) return 1;
 	
-	unsigned long errorcode=0;
-	unsigned long modenum, modelist;
-
+	//unsigned long errorcode=0;
+	//unsigned long modenum, modelist;
 	//PDC_GetTriggerModeList(1, &modenum, &modelist, &errorcode);
 	//PDC_SetExternalInMode(0, 0, PDC_EXT_IN_TRIGGER_POSI, &errorcode); 
  	//if(idpConf.setExternalInMode(1)						== PDC_FAILED) return 14; // external sync
@@ -138,11 +139,12 @@ int main(){
 	// mess start here
 	unsigned int framenum;
 	imgHead			= Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
-	// buffered images
+	imgResult		= Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
+	
 #ifdef OPT_SAVE
 	img = new cv::Mat [FRAMENUM_MAX];
 	for(framenum=0; framenum<FRAMENUM_MAX; framenum++){
-		imgHead = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
+		img[framenum] = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
 	}
 #endif
 
@@ -150,9 +152,9 @@ int main(){
 	void			*pBaseAddress;
 	
 	idpConf.writeRegister(0, 0xb4, 0); // just to be safe
-	idpConf.writeRegister(0, 0xb4, 0);
 	logintensity.open("log.txt");
 	logintensity << "start " << GetTickCount() << endl;
+	
 	
 	//while(1){
 	for (framenum=0; framenum<FRAMENUM_MAX; framenum++){
@@ -160,58 +162,63 @@ int main(){
 		if (idpConf.getLiveFrameAddress(0, &nFrameNo, &pBaseAddress) == PDC_FAILED ) break;
 		if (nFrameNo == oldFrameNo) goto waitframe;
 		oldFrameNo = nFrameNo;
-		//cout << framenum << "--" << nFrameNo << endl;
 		idpUtil.setBase((UINT8 *)pBaseAddress); // head0
 		//idpUtil.setBase((UINT8 *)pBaseAddress -8); // head1
-		//idpUtil.getHeadData(imgHead.data, 0); // head0
-		idpUtil.getHeadData(imgHead.data, 0); // head1
+		//idpUtil.getHeadData(imgHead.data, 0);
+		
+		QueryPerformanceCounter(&StartingTime);
+		idpUtil.getHeadData(imgHead.data, 0);
 		
 #ifdef OPT_SAVE
 		//for later saving
 		imgHead.copyTo(img[framenum]);
 #endif
-		// for source1
-		// current character being read
-		value_temp[0]= 0;
-		lumi_max[0]= 0;
-		lumi_min[0]= 255;
-		
-		for (int i=0; i<SECTIONNUM; i++){
-			//state[i] = FALSE;
-			lumi_led[i]= 0;
-			// selection isn't else'd so execution is rather constant
-			lumi_led[i]= imgHead.data[(axisY[i]*IMG_WIDTH + axisX[i])];
-			//if (lumi_temp[0] > lumi_threshold) state[i] = TRUE;
-			//check_lumi0();
+		// THE ROUTINE
+		background_prev= background;
+		background= TRUE;
+		for (int j=0; j<IMG_HEIGHT; j++){
+		for (int i=0; i<IMG_WIDTH; i++){
+			if (imgHead.data[j*IMG_WIDTH+i] > THRESHOLD_BACKGROUND){
+				background= FALSE;
+				if (imgHead.data[j*IMG_WIDTH+i] > THRESHOLD_LOGIC){
+					if (sequence<SEQUENCE_MAX) imgResult.data[j*IMG_WIDTH+i] |= (1<<(bitplane[sequence]));
+					}
+				}
+			}
 		}
 
-		logintensity << oldFrameNo<< ';' << short int (lumi_led[0]) \
-			<< ';' << short int (lumi_led[1]) << ';' << short int (lumi_led[2]) << ';' << short int (lumi_led[3]) \
-			<< ';' << short int (lumi_led[4]) << ';' << short int (lumi_led[5]) << ';' << short int (lumi_led[6]) \
-			<< ';' << short int (lumi_led[7]) << ';' << short int (lumi_led[8]) << ';' << short int (lumi_led[9]) \
-			<< ';' << short int (lumi_led[10]) << ';' << short int (lumi_led[11]) << ';' << short int (lumi_led[12]) \
-			<< endl;
+	QueryPerformanceCounter(&EndingTime);
+	ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+	ElapsedMicroseconds.QuadPart *= 1000000;
+	ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+	logintensity << ElapsedMicroseconds.QuadPart << endl;
+	
+	// reset sequence if all-background frame is found
+		if (background==TRUE && background_prev==FALSE){
+		sequence= 0;
+		// render the resulting image
+		imshow("result", imgResult);
+		}
+
+	// END ROUTINE
+	
 	} // framenum
 
 	logintensity << "stop  " << GetTickCount() << endl;
-	logintensity << "------------" << endl;
 	logintensity.close();
 	
 #ifdef OPT_SAVE
 	cout << "start saving" << endl;
-	vector<int> compression_params;
-	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-	compression_params.push_back(9);
-
+	
 	for(framenum=0; framenum<FRAMENUM_MAX; framenum++){
 		try {
-			sprintf(filename,"%4.4d.jpg",framenum);
+			sprintf(filename,"%4.4d.bmp",framenum);
 			// for (int i=0; i<IMG_WIDTH; i++) imgHead.data[axisY[i]*IMG_WIDTH + i] = 200;
-			// imwrite(filename, imgHead, compression_params);  // PNG
+			imwrite(filename, imgHead, compression_params);  // PNG
 			imwrite(filename, img[framenum]); // another
 		}
 		catch (runtime_error& ex) {
-			fprintf(stderr, "Exception converting image to JPG format: %s\n", ex.what());
+			fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
 			return 1;
 		}
 	}
